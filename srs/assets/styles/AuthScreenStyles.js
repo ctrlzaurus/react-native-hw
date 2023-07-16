deb44fb85489d5efd166304377cb847681c90e7f
ctrlzaurus <ichi.di@icloud.com>
2023-07-16 15:40:36 +0300

    hw3
    додано: кнопка приховати/показати пароль, валідація email, автозакриття клавіатури, по сабміту форм дані виводяться в консоль

---
+++ srs/assets/styles/AuthScreenStyles.js
@@ -0,0 +1,102 @@
+const styles = {
+    wrapper: {
+      height: "100%",
+    },
+    registrationWrapper: {
+      position: "relative",
+      marginTop: "auto",
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "100%",
+      padding: 16,
+      backgroundColor: "#fff",
+      borderTopLeftRadius: 25,
+      borderTopRightRadius: 25,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    userWrapper: {
+      width: "100%",
+      gap: 16,
+    },
+    btnAddPhoto: {
+      position: "absolute",
+      top: -60,
+      width: 120,
+      height: 120,
+      borderRadius: 16,
+      backgroundColor: '#F6F6F6',
+    },
+    btnPhoto: {
+      position: "absolute",
+      bottom: 10,
+      right: -15,
+      width: 30,
+      borderWidth: 1,
+      borderColor: '#FF6C00',
+      borderStyle: "solid",
+      borderRadius: 300,
+    },
+    singAddPhoto: {
+      color: '#FF6C00',
+      fontSize: 20,
+      textAlign: 'center',
+      paddingBottom: 1,
+    },
+    text: {
+      fontSize: 30,
+      fontWeight: 500,
+      letterSpacing: 0.3,
+      color: "#212121",
+      marginTop: 92,
+      marginBottom: 33,
+    },
+    input: {
+      width: '100%',
+      fontSize: 16,
+      color: '#212121',
+      backgroundColor: '#F6F6F6',
+      borderColor: '#E8E8E8',
+      height: 50,
+      paddingLeft: 16,
+      borderWidth: 1,
+      borderColor: "#E8E8E8",
+      borderStyle: "solid",
+      borderRadius: 4,
+    },
+    singUpBtn: {
+      width: "100%",
+      backgroundColor: "#FF6C00",
+      padding: 16,
+      borderRadius: 100,
+      marginTop: 43,
+      marginBottom: 16,
+    },
+    singUpText: {
+      fontSize: 16,
+      textAlign: "center",
+      color: "#fff",
+    },
+    singInTitle: {
+  
+    },
+    singInText: {
+      fontSize: 16,
+      fontWeight: 400,
+      color: '#1B4371',
+    },
+    passwordContainer: {
+      position: 'relative',
+    },
+    showPassword: {
+      position: 'absolute',
+      right: 16,
+      top: 13,
+    },
+    showPasswordText: {
+      fontSize: 16,
+      color: '#1B4371',
+    }
+  }
+
+export default styles;
